30920b6a3934b2c8248045ab84293b03c7f80d42
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-28 13:50:44 +0530

    Update createtab.py

--- ATM Project/createtab.py
+++ ATM Project/createtab.py
@@ -1,6 +1,6 @@
 import mysql.connector
 try:
-    con=mysql.connector.connect(host="localhost", user="root", passwd="ironman", database="NareshIT")
+    con=mysql.connector.connect(host="localhost", user="root", passwd="root", database="NareshIT")
     cur=con.cursor()
     ct="create table atm(Account_Number varchar(12) primary key, Name varchar(20) not null, Mobile_No varchar(11) not null, Balance int(20) not null, PIN varchar(5) not null)"
     cur.execute(ct)
