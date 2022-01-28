fd615ead61b45d97ef042cb8aac35efc52d984a2
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-28 13:50:25 +0530

    Update createDB.py

--- ATM Project/createDB.py
+++ ATM Project/createDB.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 con=mysql.connector.connect(host="localhost",
                     user="root",
-                    passwd="ironman")
+                    passwd="root")
 cur=con.cursor()
 cdb="create database NareshIT"
 cur.execute(cdb)
