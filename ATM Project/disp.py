2f5ca934a23922c02891fef45c21027b2e39f307
Aadarshkumar Madhukar Jadhav <96613300+aadarshkumar03@users.noreply.github.com>
2022-01-28 13:51:27 +0530

    Update disp.py

--- ATM Project/disp.py
+++ ATM Project/disp.py
@@ -1,5 +1,5 @@
 import mysql.connector
-con = mysql.connector.connect(host="localhost", user="root", passwd="ironman", database="nareshit")
+con = mysql.connector.connect(host="localhost", user="root", passwd="root", database="nareshit")
 cur = con.cursor()
 
 def disp():
